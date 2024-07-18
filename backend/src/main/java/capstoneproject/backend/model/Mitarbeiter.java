3441e518031a6d4334973263cfbb2731caa357cc
Mohammed Ly <lybhoye@yahoo.com>
2024-07-18 16:00:13 +0200

    Database connection is added

---
+++ backend/src/main/java/capstoneproject/backend/model/Mitarbeiter.java
@@ -0,0 +1,10 @@
+package capstoneproject.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@AllArgsConstructor
+@Data
+public class Mitarbeiter {
+   private String name;
+   private String id;
+}
