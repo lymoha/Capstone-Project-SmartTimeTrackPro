7e1632126adb606fefe7c0d7ccce5dbab601dd64
Mohammed Ly <lybhoye@yahoo.com>
2024-07-18 15:11:50 +0200

    add changes in src

---
+++ backend/src/main/java/capstoneproject/backend/controller/APIController.java
@@ -0,0 +1,10 @@
+package capstoneproject.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class APIController {
+
+    
+}
