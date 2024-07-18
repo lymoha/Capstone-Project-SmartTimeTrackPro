3c0b5062a0a3fe2e954de87f4106a3a44535b651
Mohammed Ly <lybhoye@yahoo.com>
2024-07-18 13:07:41 +0200

    add integration continuous

--- backend/src/test/java/capstoneproject/backend/controller/CI_InitializerTest.java
+++ backend/src/test/java/capstoneproject/backend/controller/CI_InitializerTest.java
@@ -14,4 +14,12 @@ class CI_InitializerTest {
         int actual = CI_Initializer.sum(a, b);
         assertEquals(expected, actual);
     }
+    @Test
+    void multiply_shouldReturn36_WhenCalledWith12And3() {
+        int a = 12;
+        int b = 3;
+        int expected = 36;
+        int actual = CI_Initializer.multiply(a, b);
+        assertEquals(expected, actual);
+    }
 }
